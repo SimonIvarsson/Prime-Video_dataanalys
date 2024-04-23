3bd40d77b3b74a5d7fcf9fd22bc4512a3c225ceb
SimonIvarsson <simon.ivarsson@elev.ga.ntig.se>
2024-04-23 10:58:38 +0200

    Påbörjat startmenyn, håller på med bild

--- Prime-Video-Analys.py
+++ Prime-Video-Analys.py
@@ -1,1 +1,23 @@
 import PySimpleGUI as sg
+
+sg.Theme = 'Reddit'
+
+# GUI Layout
+layout = [
+    [sg.Text('Användardata över Amazon Prime Video användare')],
+    [sg.Button('Information', expand_x = True), sg.Button('Grafer', expand_x = True), sg.Button('Tabeller', expand_x = True)],
+    [sg.Image('Small-img.png', expand_x = True)]
+]
+
+# Öppnar GUI
+window = sg.Window('Prime Video Användardata', layout)
+
+
+while True:
+    event, values = window.read()
+
+    # Om användaren trycker om close stängs GUI ner
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+
+window.close()
